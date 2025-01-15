ee01847d88f8609ac2d5c19d8619d874cea6dfdf
Ton1stark <ccastanedab@unal.edu.co>
2025-01-14 21:39:49 -0500

    EjercicioPP19.java

--- src/ejerciciopp19/EjercicioPP19.java
+++ src/ejerciciopp19/EjercicioPP19.java
@@ -3,16 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejerciciopp19;
-
-/**
- *
- * @author Dell
- */
 public class EjercicioPP19 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         VentanaPrincipal ventana = new VentanaPrincipal();
         ventana.setVisible(true);
