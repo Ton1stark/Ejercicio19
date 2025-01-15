78827ab006494e937afac78f31b78bafb828ef54
Dell <Dell@DESKTOP-H0TE2P9>
2025-01-14 20:08:50 -0500

    carga inicial

---
+++ src/ejerciciopp19/TrianguloEquilatero.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciopp19;
+
+/**
+ *
+ * @author Dell
+ */
+public class TrianguloEquilatero {
+    int lado;
+    
+    
+    TrianguloEquilatero(int lado){
+       this.lado = lado;  
+  }  
+    double obtenerArea(){
+        return (Math.pow(lado,2)*Math.sqrt(3))/4;
+    }
+    double obteneraltura(){
+        return (lado*Math.sqrt(3))/2;
+    }
+    double obtenerperimetro(){
+        return lado*3;
+    }
+}
